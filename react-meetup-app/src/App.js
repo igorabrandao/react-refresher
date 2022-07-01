62e13678443d878846a119a3869fffddba45d142
Igor Brandão <igorabrandao@gmail.com>
2022-07-01 15:15:00 -0300

    Implemented the app routing

--- react-meetup-app/src/App.js
+++ react-meetup-app/src/App.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 import AllMetupsPage from "./pages/AllMetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNaviation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNaviation />
       <Route path="/" exact>
         <AllMetupsPage />
       </Route>
