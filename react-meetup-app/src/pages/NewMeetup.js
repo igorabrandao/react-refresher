ca69117a2afd3a287f645a0a707eb2a81f33f4bd
Igor Brandão <igorabrandao@gmail.com>
2022-07-03 10:24:21 -0300

    Implemented simple fetch to store the meetup into Firebase

--- react-meetup-app/src/pages/NewMeetup.js
+++ react-meetup-app/src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-eda0d-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
