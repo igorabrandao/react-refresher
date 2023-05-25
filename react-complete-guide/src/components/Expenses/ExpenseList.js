8cedfcbac3bdfdddb992d2955c6a1828522d09b0
Igor Brandão <igorabrandao@gmail.com>
2023-05-25 15:58:44 -0300

    Adjusted the components organization

---
+++ react-complete-guide/src/components/Expenses/ExpenseList.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpenseList.css";
+import Card from "../UI/Card";
+
+function ExpenseList(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses &&
+        props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+    </Card>
+  );
+}
+
+export default ExpenseList;
