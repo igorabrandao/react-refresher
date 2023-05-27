a01e2579f3f2061d0d6b80ecc27890001156cf69
Igor Brandão <igorabrandao@gmail.com>
2023-05-27 17:28:38 -0300

    Added the year filtering component

--- react-complete-guide/src/components/Expenses/ExpenseList.js
+++ react-complete-guide/src/components/Expenses/ExpenseList.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 
 const ExpenseList = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const expenseFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        onExpenseFilterChange={expenseFilterChangeHandler}
+        selected={filteredYear}
+      />
       {props.expenses &&
         props.expenses.map((expense) => (
           <ExpenseItem
