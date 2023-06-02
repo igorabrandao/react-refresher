733aa9746842caf19e7368a88f7d3aa10be18bb9
Igor Brandão <igorabrandao@gmail.com>
2023-06-02 14:32:46 -0300

    Implemented the cartReducer to handle the item ADD action

---
+++ react-food-delivery-app/src/store/CartProvider.js
@@ -0,0 +1,57 @@
+import { useReducer } from "react";
+
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // Concatenate the new item with the existing items
+    const updatedItems = state.items.concat(action.item);
+
+    // Calculate the new total amount
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    // Return the updated state
+    return {
+      ietms: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
