5c2261c0e6f91572e88e0c995f808ea2c2965851
Igor Brandão <igorabrandao@gmail.com>
2023-06-13 17:41:54 -0300

    Added a new project to use Class based components

---
+++ react-classes-app/src/components/Users.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+
+import User from "./User";
+import classes from "./Users.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class Users extends Component {
+  constructor() {
+    // Call the constructor of the Component class
+    super();
+
+    // Class state
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  toggleUsersHandler() {
+    // this.state.showUsers = false; // NOT!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+export default Users;
