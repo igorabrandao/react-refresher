d6f34f6ad8c9e5d6e8ea5015ce3b5fb172dc292e
Igor Brandão <igorabrandao@gmail.com>
2023-06-21 15:05:32 -0300

    Applied the custom hook to send POST request on NewTask module

--- react-realistic-custom-hook-app/src/components/NewTask/NewTask.js
+++ react-realistic-custom-hook-app/src/components/NewTask/NewTask.js
@@ -1,41 +1,31 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttpRequest from "../../hooks/use-http-request";
+
+const BASE_URL = "https://react-http-6205b-default-rtdb.firebaseio.com";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttpRequest();
+
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-6205b-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: `${BASE_URL}/tasks.json`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createdTask.bind(null, taskText)
+    );
   };
 
   return (
