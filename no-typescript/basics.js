b7bf5957254e0d99d7722cb20302ca27ca3bd868
Igor Brandão <igorabrandao@gmail.com>
2023-06-21 17:58:17 -0300

    Added examples using generic types

--- no-typescript/basics.js
+++ no-typescript/basics.js
@@ -1,6 +1,15 @@
 // Primite types: number, string, boolean, null, undefined, void, symbol
 // More complex types: arrays, objects
 // Function types, parameters
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // Primitive types
 // ========================================================
 var age = 24;
@@ -38,3 +47,14 @@ function add(a, b) {
 function printValue(value) {
     console.log(value);
 }
+// Generics
+// ========================================================
+function insertAtBeginning(array, value) {
+    var newArray = __spreadArray([value], array, true);
+    return newArray;
+}
+var demoArray = [1, 2, 3];
+var updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+var stringArray = insertAtBeginning(["a", "b", "c"], "d");
+var booleanArray = insertAtBeginning([true, false, false], true);
+console.log(updatedArray, stringArray, booleanArray);
--- no-typescript/basics.ts
+++ no-typescript/basics.ts
@@ -54,3 +54,21 @@ function add(a: number, b: number) {
 function printValue(value: any) {
   console.log(value);
 }
+
+// Generics
+// ========================================================
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+const booleanArray = insertAtBeginning([true, false, false], true);
+// const booleanArray2 = insertAtBeginning([true, false, false], "true"); // error
+
+console.log(updatedArray, stringArray, booleanArray);
+
+// With the generic type, we can use functions for specific types
+// stringArray[0].split(""); // no error
