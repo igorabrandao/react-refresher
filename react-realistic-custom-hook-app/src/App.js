7e694db8a6388b70f6e722cc41fa57acda813641
Igor Brandão <igorabrandao@gmail.com>
2023-06-21 14:54:49 -0300

    Refactored the custom Hook and the App.js file to reduce dependencies

--- react-realistic-custom-hook-app/src/App.js
+++ react-realistic-custom-hook-app/src/App.js
@@ -8,26 +8,21 @@ const BASE_URL = "https://react-http-6205b-default-rtdb.firebaseio.com";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttpRequest({ url: `${BASE_URL}/tasks.json` }, transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttpRequest();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({ url: `${BASE_URL}/tasks.json` }, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
