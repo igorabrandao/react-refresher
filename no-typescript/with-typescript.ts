5c4a1f63ae4333a4bb8920ccc0cec8d73b0bca44
Igor Brandão <igorabrandao@gmail.com>
2023-06-21 17:19:16 -0300

    Created a dummy project to test Typescript

---
+++ no-typescript/with-typescript.ts
@@ -0,0 +1,10 @@
+// To use typescript, you need to install it first:
+// npm init -y
+// npm install typescript or npm install -g typescript
+// npx tsc with-typescript.ts
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(2, 5));
