da0463ae25ab4073f52acc66469663f6c649768d
Igor Brandão <igorabrandao@gmail.com>
2023-06-22 17:03:47 -0300

    Added styling to the app

--- react-typescript-app/src/components/NewTodo.tsx
+++ react-typescript-app/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 
+import classes from "./NewTodo.module.css";
+
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
@@ -17,7 +19,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="todo-text">Todo Text</label>
       <input type="text" id="todo-text" ref={todoTextInputRef} />
       <button type="submit">Add Todo</button>
