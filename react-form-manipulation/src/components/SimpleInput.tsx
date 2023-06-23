d869f9a76c7a386dd6e51c146adc77544997a5e9
Igor Brandão <igorabrandao@gmail.com>
2023-06-23 14:43:12 -0300

    Added a simple validation

--- react-form-manipulation/src/components/SimpleInput.tsx
+++ react-form-manipulation/src/components/SimpleInput.tsx
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 
 const SimpleInput: React.FC = (props) => {
+  // Ref can be used to get the value of an input field on submit
   const inputNameRef = useRef<HTMLInputElement>(null);
+
+  // State can be used to get the value of an input field on change
   const [enteredName, setEnteredName] = useState<string>("");
 
   const nameInputChangeHandler = (
@@ -12,6 +15,10 @@ const SimpleInput: React.FC = (props) => {
 
   const formSubmissionHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    if (enteredName.trim().length === 0) {
+      return;
+    }
 
     console.log(enteredName);
     const enteredValue = inputNameRef.current!.value;
