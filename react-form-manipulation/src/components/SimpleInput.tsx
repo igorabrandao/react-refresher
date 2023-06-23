3d99d3e5882191d2dc248e8341d1efcd20eb815a
Igor Brandão <igorabrandao@gmail.com>
2023-06-23 14:31:02 -0300

    Implemented different ways of acquiring value from input

--- react-form-manipulation/src/components/SimpleInput.tsx
+++ react-form-manipulation/src/components/SimpleInput.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const SimpleInput: React.FC = (props) => {
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const [enteredName, setEnteredName] = useState<string>("");
+
+  const nameInputChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = inputNameRef.current!.value;
+    console.log(enteredValue);
+
+    // Reset the input field
+    setEnteredName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          ref={inputNameRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
