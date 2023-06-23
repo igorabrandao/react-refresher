6ecb2340e2d64a9378791676265687f29925e6fb
Igor Brandão <igorabrandao@gmail.com>
2023-06-23 11:58:52 -0300

    Implemented the Todo context and made it work with typescript

--- react-typescript-app/src/App.tsx
+++ react-typescript-app/src/App.tsx
@@ -1,29 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    setTodos((prevState) => {
-      return [...prevState, new Todo(todoText)];
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevState) => {
-      return prevState.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
---
+++ react-typescript-app/src/store/todos-context.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import Todo from "../models/todo";
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    setTodos((prevState) => {
+      return [...prevState, new Todo(todoText)];
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevState) => {
+      return prevState.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
