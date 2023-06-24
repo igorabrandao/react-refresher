2a58b436229edc78ac3a3c8b696a3612aaf19caf
Igor Brandão <igorabrandao@gmail.com>
2023-06-24 13:09:22 -0300

    Refactored the new custom hook to apply useReducer

--- react-form-manipulation/src/hooks/use-input2.ts
+++ react-form-manipulation/src/hooks/use-input2.ts
@@ -1,30 +1,64 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+enum InputActionType {
+  Input = "INPUT",
+  Blur = "BLUR",
+  Reset = "RESET",
+}
+
+type InputState = {
+  value: string;
+  isTouched: boolean;
+};
+
+type InputAction = {
+  type: InputActionType;
+  value: string;
+};
+
+const initialInputState: InputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state: InputState, action: InputAction) => {
+  switch (action.type) {
+    case InputActionType.Input:
+      return { value: action.value, isTouched: state.isTouched };
+    case InputActionType.Blur:
+      return { isTouched: true, value: state.value };
+    case InputActionType.Reset:
+      return { isTouched: false, value: "" };
+    default:
+      return state;
+  }
+};
 
 const useInput2 = (validationFn: (value: string) => boolean) => {
-  const [enteredValue, setEnteredValue] = useState<string>("");
-  const [isTouched, setIsTouched] = useState<boolean>(false);
-  
-  const inputIsValid = validationFn(enteredValue);
-  const hasError = !inputIsValid && isTouched;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+  const inputIsValid = validationFn(inputState.value);
+  const hasError = !inputIsValid && inputState.isTouched;
 
   // Handle the input change, blur, reset event
   const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: InputActionType.Input, value: event.target.value });
   };
 
   const inputBlurHandler = (event: React.FocusEvent<HTMLInputElement>) => {
-    setIsTouched(true);
+    dispatch({ type: InputActionType.Blur, value: "" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: InputActionType.Reset, value: "" });
   };
 
   // return the value and the event handlers
   return {
-    value: enteredValue,
-    isTouched,
+    value: inputState.value,
+    isTouched: inputState.isTouched,
     hasError,
     isValid: inputIsValid,
     valueChangeHandler,
